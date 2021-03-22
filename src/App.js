a44b30ba5700f113e2c07a703d548742293b7445
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-03-22 14:46:31 +0100

    base added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import LoadingMask from "./components/LoadingMask";
+import mockerserver from "./api/mockserver";
 
-const App = () => {
+function App() {
+  const [hotels, setHotels] = useState(null);
+
+  useEffect(() => {
+    fetch(makeServer())
+      .then((response) => response.json())
+      .then((data) => {
+        setHotels(data);
+        console.log(data);
+      })
+      .catch((error) => console.log("Error:", error));
+  }, []);
+
+  const data = [
+    {
+      name: "Hotel Nulla sit amet",
+      stars: 3,
+      city: "Los Angeles",
+    },
+    {
+      name: "Hotel Curabitur suscipit suscipit",
+      stars: 4,
+      city: "Las Vegas",
+    },
+    {
+      name: "Hotel Donec id justo",
+      stars: 5,
+      city: "San Francisco",
+    },
+  ];
 
   return (
     <div className="App">
-      Todo...
+      <h1>Hotels</h1>
+      {hotels === null ? <LoadingMask /> : ""}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LoadingMask.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoadingMask() {
+  return <div className="loading-mask">Loading...</div>;
+}
+
+export default LoadingMask;
