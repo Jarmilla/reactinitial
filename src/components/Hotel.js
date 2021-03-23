aeb07ca7b80e7007f2f1f1a79773bb62081b8119
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-03-23 13:15:01 +0100

    validation

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 function Hotel({ hotel }) {
   const [isShown, setIsShown] = useState(false);
+  const [isEmaiInputShown, setIsEmaiInputShown] = useState(false);
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [email, setEmail] = useState("");
+
+  function validation(str) {
+    if (str.includes("@") && str.includes(".")) return setIsValidEmail(true);
+  }
 
   return (
     <div className="hotel">
@@ -12,7 +20,23 @@ function Hotel({ hotel }) {
           <p>
             {hotel.city} ({hotel.stars})
           </p>
-          <button>Request more info about {hotel.name}</button>
+          <button onClick={() => setIsEmaiInputShown(!isEmaiInputShown)}>Request more info about {hotel.name}</button>
+        </div>
+      )}
+      {isEmaiInputShown && (
+        <div>
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              console.log(email);
+              validation(email);
+            }}
+            type="email"
+          />
+          <button disabled={isValidEmail ? false : true} required>
+            Submit
+          </button>
         </div>
       )}
     </div>
